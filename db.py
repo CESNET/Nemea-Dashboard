26715a7d5e86eb9262be2c19b37e6dd03307d9dd
Stehlík Petr <xstehl14@benefizio.liberouter.org>
2015-08-04 08:47:37 +0200

    Experiments with pymongo

--- db.py
+++ db.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3.4
+
 import pymongo
+import time
+from datetime import date
+from time import mktime
 
 # Connection to Mongo DB
 try:
@@ -10,13 +15,23 @@ except pymongo.errors.ConnectionFailure:
 db = conn.test_corr_db
 print(conn.database_names())
 
+
+t = time.process_time()
+
 collection = db.events
 
 #'type': 'PORTSCAN_H'
 counter = {}
 
-for doc in collection.find()[:1000]:
-#	print(doc['type'])
+start = date(2015, 7, 3)
+unixtime = int(mktime(start.timetuple()))
+#collection.create_index([( "scale", -1)])
+#collection.create_index([( "time_first" , 1)])
+#collection.drop_index([( "scale", -1)])
+#collection.drop_index([( "time_first" , 1)])
+for doc in collection.find( { "time_first": {"$gt" : unixtime }, "type": "PORTSCAN_H" }).sort( [( "scale", -1)] ).limit(10):
+#for doc in collection.find().sort([( "$natural", -1)]).limit(10):
+	print(doc)
 	doctype = doc['type']
 	#tmp = counter[doctype]
 	if doctype in counter:
@@ -28,3 +43,7 @@ for doc in collection.find()[:1000]:
 #		counter += 1
 
 print(counter)
+
+print(time.process_time() - t)
+#print(int(time.time()))
+#print(mktime(start.timetuple()))
