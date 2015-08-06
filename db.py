0dcf0ef3440d5ea7a3aaa03497204a24f2662ae0
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2015-08-06 20:15:08 +0200

    First tries in API

--- db.py
+++ db.py
@@ -1,35 +1,69 @@
-#!/usr/bin/python3.4
+#!/usr/bin/env python3
 
 import pymongo
 import time
+import sys
 from datetime import date
 from time import mktime
 
-# Connection to Mongo DB
-try:
-    conn=pymongo.MongoClient()
-    print ("Connected successfully!!!")
-except pymongo.errors.ConnectionFailure, e:
-        print "Could not connect to server: %s" % e
+from eve import Eve
+app = Eve()
 
-db = conn.test_corr_db
-print(conn.database_names())
+if __name__ == '__main__':
+    app.run()
+
+class db(object):
+
+	host = "localhost"
+	port = 27017
+	dbName = "test_corr_db"
+	collectionName = "events"
+	collection = None
+	db = None
+	socket = None
+
+	def __init__(self, host = None, port = None, db = None, collection = None):
+
+		if host != None and port != None:
+			self.host = host
+			self.port = port
+		
+			if db != None and collection != None:
+				self.dbName = db
+				self.collectionName = collection
+
+		try:
+			self.socket = pymongo.MongoClient(self.host, self.port)
+			self.db = self.socket[self.dbName]
+			self.collection = self.db[self.collectionName]
+		except Exception:
+			print("Cannot establish connection to database")
+			sys.exit()
+		
+	def indexes(self):
+		self.collection.create_index([( "time_first" , 1)])
+		print("time_first index created")
 
 
-t = time.process_time()
+mongo = db();
+#print(mongo.socket.server_info())
 
-collection = db.events
+print(mongo.socket.database_names())
 
-#'type': 'PORTSCAN_H'
+mongo.indexes()
+
+sys.exit()
+
+
 counter = {}
 
 start = date(2015, 7, 3)
 unixtime = int(mktime(start.timetuple()))
 #collection.create_index([( "scale", -1)])
-#collection.create_index([( "time_first" , 1)])
+#conn.collection.create_index([( "time_first" , 1)])
 #collection.drop_index([( "scale", -1)])
 #collection.drop_index([( "time_first" , 1)])
-for doc in collection.find( { "time_first": {"$gt" : unixtime }, "type": "PORTSCAN_H" }).sort( [( "scale", -1)] ).limit(10):
+for doc in mongo.collection.find( { "time_first": {"$gt" : unixtime }, "type": "PORTSCAN_H" }).sort( [( "scale", 1)] ).limit(10):
 #for doc in collection.find().sort([( "$natural", -1)]).limit(10):
 	print(doc)
 	doctype = doc['type']
@@ -44,6 +78,5 @@ for doc in collection.find( { "time_first": {"$gt" : unixtime }, "type": "PORTSC
 
 print(counter)
 
-print(time.process_time() - t)
 #print(int(time.time()))
 #print(mktime(start.timetuple()))
