f6e56c8abd7fae7e22b490f60c958eff563d2c62
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2015-08-17 13:15:46 +0200

    API /events/last/n API /events/createindex

--- api/run.py
+++ api/run.py
@@ -32,7 +32,7 @@ class db(object):
 
 		except Exception:
 			print("Cannot establish connection to database")
-			#sys.exit()
+			sys.exit()
 		
 	def indexes(self):
 		self.collection.create_index([( "time_first" , 1)])
@@ -43,14 +43,28 @@ class db(object):
 app = Flask(__name__)
 app.debug = True
 
+mongo = db()
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/events/createindex')
+def create_index():
+	
+	#for index in mongo.collection.list_indexes():
+	#    print(index)
+	indexes = mongo.collection.index_information()
+	for item in indexes.iterkeys():
+		if item == "scale_1":
+			print('index scale is here')
+		else:
+			mongo.collection.create_index([( "scale", 1)])
+		#print(item)
+	return(json.dumps(indexes))
+
 @app.route('/events')
 def connect_db():
-	mongo = db()
-	mongo.collection.create_index([( "scale", -1)])
 	doctype = {}
 
 	print('lets rock')
@@ -80,6 +94,22 @@ def connect_db():
 			counter[doctype] = 1 
 
 	return str(foo)
+
+@app.route('/events/last/<items>')
+def get_last(items):
+	docs = mongo.collection.find().sort( [( "$natural", -1)] ).limit(int(items))
+	docs_dict = {}
+	i = 0
+
+	for doc in docs:
+		#print(doc)
+		tmp = "item_" + str(i)
+		docs_dict[tmp] = doc
+		i += 1
+		
+	return(json.dumps(docs_dict))
+	#return(json.dumps(docs_dict, sort_keys=True, indent=4))
+	#return ""
 
 if __name__ == '__main__':
     app.run()
