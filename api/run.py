5511d46a666e8b62e7a75fb8f98ad9e72204671d
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2015-08-17 15:52:43 +0200

    Created new virtualenv for development in python3 and extended base API

--- api/run.py
+++ api/run.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, redirect
 import pymongo
 import json
 import sys
 from datetime import date
 from time import mktime
+
+from bson import Binary, Code
+from bson.json_util import dumps
 
 class db(object):
 
@@ -33,10 +36,6 @@ class db(object):
 		except Exception:
 			print("Cannot establish connection to database")
 			sys.exit()
-		
-	def indexes(self):
-		self.collection.create_index([( "time_first" , 1)])
-		print("time_first index created")
 
 
 
@@ -45,22 +44,46 @@ app.debug = True
 
 mongo = db()
 
+def parse_doc(docs):
+	tmp = []
+
+	for doc in docs:
+		tmp.append(doc)
+	
+	return(dumps(tmp, sort_keys=True, indent=4))
+
+def get_event(event_type, limit):
+	if limit == 0:
+		return("You cannot dump the whole DB")
+	else:
+		return(mongo.collection.find( {"type" : event_type.upper() } ).sort( [( "$natural", 1)] ).limit(limit))
+
+def get_event_type(event_type):
+	if event_type == "portscan":
+		return "PORTSCAN_H"
+	elif event_type == "voipguess":
+		return "VOIP_PREFIX_GUESS"
+	elif event_type == "voipcall":
+		return "VOIP_CALL_DIFFERENT_COUNTRY"
+	elif event_type == "dnstunnel":
+		return "DNS_TUNNEL"
+	else:
+		return event_type
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/events/createindex')
 def create_index():
-	
-	#for index in mongo.collection.list_indexes():
-	#    print(index)
 	indexes = mongo.collection.index_information()
-	for item in indexes.iterkeys():
-		if item == "scale_1":
+	for item in indexes.keys():
+		if item == "scale_1" or item == "type_1":
 			print('index scale is here')
 		else:
 			mongo.collection.create_index([( "scale", 1)])
-		#print(item)
+			mongo.collection.create_index([( "type", 1)])
+
 	return(json.dumps(indexes))
 
 @app.route('/events')
@@ -95,21 +118,70 @@ def connect_db():
 
 	return str(foo)
 
-@app.route('/events/last/<items>')
-def get_last(items):
-	docs = mongo.collection.find().sort( [( "$natural", -1)] ).limit(int(items))
-	docs_dict = {}
-	i = 0
+#############################
+#Get the last event
+#############################
+@app.route('/events/last/')
+def redirect_last():
+	return(get_last(1))
 
-	for doc in docs:
-		#print(doc)
-		tmp = "item_" + str(i)
-		docs_dict[tmp] = doc
-		i += 1
+#############################
+#Get last n events
+#############################
+@app.route('/events/last/<int:items>')
+def get_last(items):
+	if items != 0:
+		docs = mongo.collection.find().sort( [( "$natural", -1)] ).limit(items)
+	else:
+		return("You cannot dump the whole DB!")
 		
-	return(json.dumps(docs_dict))
-	#return(json.dumps(docs_dict, sort_keys=True, indent=4))
-	#return ""
+	temp = parse_doc(docs)
+
+	print(temp)
+	return (temp)
+
+#############################
+#Get the first event
+#############################
+@app.route('/events/first/')
+def get_first_item():
+	return (get_first(1))
+
+#############################
+#Get first n events
+#############################
+@app.route('/events/first/<int:items>')
+def get_first(items):
+	if items != 0:
+		docs = mongo.collection.find().sort( [( "$natural", 1)] ).limit(items)
+	else:
+		return("You cannot dump the whole DB!")
+	
+	temp = parse_doc(docs)
+
+	#debug purposes
+	#print(temp)
+
+	return (temp)
+
+@app.route('/events/type/<event_type>/')
+def get_event_item(event_type):
+
+	docs = get_event(get_event_type(event_type), 1)
+
+	return(parse_doc(docs))
+
+@app.route('/events/type/<event_type>/last/<int:limit>')
+def get_type(event_type, limit):
+	docs = get_event(get_event_type(event_type), limit)
+
+	return(parse_doc(docs))
+
+@app.route('/events/type/<event_type>/top/<int:limit>')
+def get_top_events(event_type, limit):
+	docs = mongo.collection.find( {"type" : get_event_type(event_type).upper() } ).sort( [( "scale", -1)] ).limit(limit)
+
+	return(parse_doc(docs))
 
 if __name__ == '__main__':
     app.run()
