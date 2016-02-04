ad12a23be2862ccbd6ddd8f3d6c23fc831529dd9
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-04 16:37:00 +0100

    CLN: moved constats to correct file

--- js/app.js
+++ js/app.js
@@ -100,17 +100,4 @@ app.filter('nospace', function () {
       };
     });
 
-// CONSTANTS
 
-app.constant('AUTH_EVENTS', {
-  loginSuccess: 'auth-login-success',
-  loginFailed: 'auth-login-failed',
-  logoutSuccess: 'auth-logout-success',
-  sessionTimeout: 'auth-session-timeout',
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
-});
-
-app.constant('CATEGORIES', [ "Recon.Scanning", "Attempt.Login", "Availability.DoS" ]);
-app.constant('PROTOCOLS', [ "tcp", "dns", "udp" ]);
-app.constant('TYPES', ["histogram", "piechart"])
--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,16 @@
+app.constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+});
+
+app.constant('CATEGORIES', [ "Recon.Scanning", "Attempt.Login", "Availability.DoS" ]);
+app.constant('PROTOCOLS', [ "tcp", "dns", "udp" ]);
+app.constant('TYPES', ["barchart", "piechart"])
+
 app.constant('PIECHART', { 
     options: {
         chart: {
@@ -7,27 +20,39 @@ app.constant('PIECHART', {
             y: function(d){return d.x;},
             showLabels: true,
             donut : true,
-            padAngle : 0.02,
-            cornerRadius : 3,
+            padAngle : 0.00,
+            cornerRadius : 1,
             transitionDuration: 500,
-            labelThreshold: 0.1,
-            color: ["#0ec4f4", "#631FF6", "#FFDC06", "#FF8406", "#b56969", "#e6cf8b"],
+            labelThreshold: 0,
+            //color: ["#0ec4f4", "#631FF6", "#FFDC06", "#FF8406", "#b56969", "#e6cf8b"],
             legend: {
                 margin: {
-                    top: 0,
-                    right: 100,
-                    bottom: 5,
+                    top: 5,
+                    right: 0,
+                    bottom: 0,
                     left: 0
                 }
             },
-pie : {
-            dispatch: {
-                //chartClick: function(e) {console.log(e)},
-                elementClick: function(e) {console.log(e)},
-                //elementDblClick: function(e) {console.log("element double click")},
-                //elementMouseover: function(e) {console.log("element mouseover")},
-                //elementMouseout: function(e) {console.log("element mouse out")}            
-            },}
+            tooltipContent : function(key, x, y, e, graph) {
+                                console.log(key);
+                                console.log(y);
+                                console.log(e);
+                                console.log(graph);
+                                return 'Olalala!';
+                             },
+            pie : {
+                dispatch: {
+                    //chartClick: function(e) {console.log(e)},
+                    elementClick: function(e) {console.log(e)},
+                    //elementDblClick: function(e) {console.log("element double click")},
+                    //elementMouseover: function(e) {console.log("element mouseover")},
+                    //elementMouseout: function(e) {console.log("element mouse out")}            
+                },
+                labelType : "percent",
+                labelsOutside : true,
+                startAngle : function(d) { return d.startAngle/2 -Math.PI/2 },
+                endAngle : function(d) { return d.endAngle/2 -Math.PI/2 }
+            }
         }
     }
 });
@@ -39,10 +64,10 @@ app.constant('AREA', {
             type: 'multiBarChart',
             height: 500,
             margin : {
-                top: 20,
+                top: 30,
                 right: 20,
-                bottom: 30,
-                left: 40
+                bottom: 100,
+                left: 50
             },
             x: function(d) { return d.x },
             y: function(d) { return d.Count },
@@ -53,13 +78,14 @@ app.constant('AREA', {
             xAxis: {
                 showMaxMin: false,
                 tickFormat: function(d) {
-                    return d3.time.format('%X')(new Date(d))
-                }
+                    return d3.time.format('%m/%d %H:%M')(new Date(d))
+                },
+                rotateLabels : -45,
             },
             yAxis: {
-                //tickFormat: function(d){
-                //    return d3.format(',.2f')(d);
-               // }
+                tickFormat: function(d){
+                    return d3.format(',.0f')(d);
+                }
             },
             zoom: {
                 enabled: true,
