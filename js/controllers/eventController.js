8f4ebc88435a7eb8a3c2919b2287ef6219fa3ea0
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-09 18:11:11 +0100

    ADD: GeoIP and return functionality

--- js/controllers/eventController.js
+++ js/controllers/eventController.js
@@ -2,15 +2,43 @@ app.controller('eventController', function($scope, $routeParams, $http, api) {
 
         $scope.id = $routeParams;
 
+        $scope.geo = [];
+
+        $scope.back = function() {
+            window.history.back()
+        }
+
         api.get('id/' + $routeParams['id']).success(function(data) {
-            $scope.data = data;    
-            if ($scope.data.Target == undefined || $scope.data.Target[0].IP4 == undefined) {
+            $scope.data = data;
+
+            //var bounds = new google.maps.LatLngBounds();
+
+            if (($scope.data.Source != undefined && $scope.data.Target != undefined) && 
+                ($scope.data.Source[0].IP4 && $scope.data.Target[0].IP4)) {
+                $http.get('http://freegeoip.net/json/' + $scope.data.Target[0].IP4[0]).success(function(data) {
+                    
+                    data['type'] = "Target";
+                    $scope.geo.push(data);
+                    //var latlng = new google.maps.LatLng(data.latitude, data.longitude);
+                    //bounds.extend(latlng);
+
+                })
                 $http.get('http://freegeoip.net/json/' + $scope.data.Source[0].IP4[0]).success(function(data) {
-                    $scope.geo = data;
+                    data['type'] = "Source";
+                    $scope.geo.push(data);
+                    //var latlng = new google.maps.LatLng(data.latitude, data.longitude);
+                    //bounds.extend(latlng);
+                })
+            }
+            else if ($scope.data.Source == undefined || $scope.data.Source[0].IP4 == undefined) {
+                $http.get('http://freegeoip.net/json/' + $scope.data.Target[0].IP4[0]).success(function(data) {
+                    data['type'] = "Target";
+                    $scope.geo.push(data);
                 })
             } else {
-                $http.get('http://freegeoip.net/json/' + $scope.data.Target[0].IP4[0]).success(function(data) {
-                    $scope.geo = data;
+                $http.get('http://freegeoip.net/json/' + $scope.data.Source[0].IP4[0]).success(function(data) {
+                    data['type'] = "Source";
+                    $scope.geo.push(data);
                 })
             }
         })
