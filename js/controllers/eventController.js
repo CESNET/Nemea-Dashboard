6d15242be120cf5b2bca9efb57fa4bbd93d462b4
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-10 14:14:09 +0100

    ADD: IDEA format filter

--- js/controllers/eventController.js
+++ js/controllers/eventController.js
@@ -72,3 +72,22 @@ app.filter('json2html', function($sce, $filter) {
         return $sce.trustAs('html', html);
     }     
 });
+
+// Filter to convert IDEA format from MongoDB to standardized IDEA format
+app.filter('idea', function() {
+    return function(input) {
+        var res = {};
+
+        angular.forEach(input, function(value, key) {
+            if (key != '_id') {
+                if (value['$date']) {
+                    res[key] = new Date(value['$date']).toISOString().slice(0,19) + 'Z';
+                } else {
+                    res[key] = value;
+                }
+            }
+        });
+
+        return res;
+    }
+});
