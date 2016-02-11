b8d0b6caba37b782c5cc78a8c0a4829c8be867f3
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-11 09:23:36 +0100

    Moved MENU to constants.js

--- js/constants.js
+++ js/constants.js
@@ -111,3 +111,64 @@ app.constant('AREA', {
         }
     }
 });
+
+
+
+app.constant("MENU", [
+	{
+		"title" : "Dashboard",
+        "link" : "/"
+		/*"items" : [
+			{
+				"title" : "Users",
+				"link"	: "user"
+			},
+			{
+				"title" : "My profile"
+			}
+		]*/
+	},
+	{
+		"title" : "Events",
+		"link" 	: "/events"
+	},
+	/*{
+		"title" : "Analytics23",
+		"items" : [
+			{
+				"title" : "Users",
+				"link" 	: "settings"
+			},
+			{
+				"title" : "My profile",
+				"link"	: "profile"
+			},
+			{
+				"title" : "My profile",
+				"link"	: "profile"
+			}
+		]
+
+	},
+	{
+		"title" : "Analytics123",
+		"link" 	: "login"
+	},*/
+	{
+		"title" : "Settings",
+		"items" : [
+			{
+				"title" : "Users",
+				"link" 	: "/settings"
+			},
+			{
+				"title" : "My profile",
+				"link"	: "/profile"
+			},
+			{
+				"title" : "My profile",
+				"link"	: "/profile"
+			}
+		]
+	}
+]);
--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -152,64 +152,6 @@ app.controller('homeController', function($scope, $log, api, boxes_arr, $http, $
 });
 
 
-app.constant("MENU", [
-	{
-		"title" : "Dashboard",
-        "link" : "/"
-		/*"items" : [
-			{
-				"title" : "Users",
-				"link"	: "user"
-			},
-			{
-				"title" : "My profile"
-			}
-		]*/
-	},
-	{
-		"title" : "Events",
-		"link" 	: "/events"
-	},
-	/*{
-		"title" : "Analytics23",
-		"items" : [
-			{
-				"title" : "Users",
-				"link" 	: "settings"
-			},
-			{
-				"title" : "My profile",
-				"link"	: "profile"
-			},
-			{
-				"title" : "My profile",
-				"link"	: "profile"
-			}
-		]
-
-	},
-	{
-		"title" : "Analytics123",
-		"link" 	: "login"
-	},*/
-	{
-		"title" : "Settings",
-		"items" : [
-			{
-				"title" : "Users",
-				"link" 	: "/settings"
-			},
-			{
-				"title" : "My profile",
-				"link"	: "/profile"
-			},
-			{
-				"title" : "My profile",
-				"link"	: "/profile"
-			}
-		]
-	}
-]);
 
 
 
