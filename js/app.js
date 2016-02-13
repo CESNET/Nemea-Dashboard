d59baeeb803811d83cfde439389b170fd969fb5d
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-13 17:48:54 +0100

    Response error handling with promise return

--- js/app.js
+++ js/app.js
@@ -7,23 +7,14 @@ function config($routeProvider, $mdThemingProvider, $httpProvider, $localStorage
 		.when('/login', {
 			controller: 'loginController',
 			templateUrl: 'views/login.html',
-			resolve: {
-				//isLogin: checkLogin
-			}
 		})
 		.when('/', {
 			controller: 'homeController',
 			templateUrl: 'views/home.html',
-			resolve: {
-				//isLogin: checkLogin
-			}
 		})
 		.when('/user', {
 			controller: 'userController',
 			templateUrl: 'views/login.html',
-			resolve: {
-				//isLogin: checkLogin
-			}
 		})
         .when('/events', {
             controller : 'eventsController',
@@ -43,7 +34,6 @@ function config($routeProvider, $mdThemingProvider, $httpProvider, $localStorage
     $httpProvider.interceptors.push('notAllowedInterceptor');
     $localStorageProvider
         .setKeyPrefix('nd-');
-//	$httpProvider.defaults.headers.common['Authorization'] = "autentizace";
 	// localStorageServiceProvider
 	// 	.setPrefix('nemea-dashboard')
 	// 	//PRODUCTION
@@ -55,49 +45,6 @@ function config($routeProvider, $mdThemingProvider, $httpProvider, $localStorage
 //	$locationProvider.html5Mode(true);
 };
 
-// $rootScope.$on('$routeChangeStart', function (event, next) {
-//         var userAuthenticated = false; /* Check if the user is logged in */
-
-//         if (!userAuthenticated && !next.isLogin) {
-//             $rootScope.savedLocation = $location.url();
-
-//             $location.path('/login');
-//         }
-//     });
-
-var loginCorrect = null;
-
-checkLogin = function($rootScope, $location, localStorageService, $rootScope) {
-	// console.log("login: " + loginCorrect);
-	// console.log(localStorageService.keys());
-	// if (localStorageService.keys() == '') {
-	// 	localStorageService.set('loggedIn', null);
-	// }
-
-	//$rootScope.$on("$locationChangeStart", function(event){
-
-    	// if (localStorageService.get("loggedIn")) {
-    	// 	//$location.path("/");
-    	// 	console.log("correct")	
-    	// }
-    	// else {
-    	// 	$location.path("/login");
-    	// }
-	//})
-
-	$rootScope.$on("$locationChangeStart", function(event){
-		if (localStorageService.get("loggedIn")) {
-    		//$location.path("/");
-    		console.log("correct")	
-    	}
-    	else {
-    		$location.path("/login");
-    	}
-    })
-
-	return false;
-}
-
 //take all whitespace out of string
 app.filter('nospace', function () {
       return function (value) {
@@ -105,7 +52,7 @@ app.filter('nospace', function () {
       };
     });
 
-
+// Always check for a JWT
 app.run(function(user, $localStorage, $location, $rootScope, $log) {
 	$rootScope.$on("$locationChangeStart", function(event) {
         if ($localStorage["token"] == undefined) {
@@ -115,22 +62,22 @@ app.run(function(user, $localStorage, $location, $rootScope, $log) {
     })
 })
 
-
-app.factory('notAllowedInterceptor', function($log, $localStorage, $location, $injector) {
+// HTTP interceptor injected in config
+app.factory('notAllowedInterceptor', function($log, $localStorage, $location, $injector, $q) {
     var notAllowedInterceptor = {
+        // Intercept $http errors
         responseError : function(response) {
-            var $http = $injector.get('$http');
+            // Check for 401 error
             if (response.status == 401) {
-                $log.error('You are not allowed to access');
-                $log.error(response);
+                $log.error('You are not allowed to access, redirecting to /login');
+                // Delete exisiting JWT from localStorage 
                 delete $localStorage["token"];
+                // Redirect to login
                 $location.path('/login');
             }
-
-            
-            return $http(response);
+            // Return promise so we can handle the error further
+            return $q.reject(response);
         }
     }
-
     return notAllowedInterceptor;
 })
