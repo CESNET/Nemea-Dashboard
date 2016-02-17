ee9044f7757b6d1d016162c24d28e5eb3e6d7b40
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-02-17 18:04:47 +0100

    New tasks for bower

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var sourcemaps = require('gulp-sourcemaps')
 var uglify = require('gulp-uglify')
 var ngAnnotate = require('gulp-ng-annotate')
 var sass = require('gulp-sass')
-var mainBowerFiles = require('main-bower-files');
+var gulpFilter = require('gulp-filter')
+var bower = require('main-bower-files');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('js', function () {
 gulp.src(['js/**/*.js', '!js/**/*.min.js'])
@@ -35,4 +37,46 @@ gulp.task('production', function() {
         .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(gulp.dest('.'))
+})
+
+var dist = {
+    js : 'public/',
+    css : 'public/',
+    vendor : 'public/'
+}
+
+
+gulp.task('bower', function() {
+    var jsFilter = gulpFilter('**/*.js')
+    var cssFilter = gulpFilter('**/*.css')
+    return gulp.src(bower())
+        .pipe(jsFilter)
+        .pipe(concat('vendor.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dist.js))
+        //.pipe(jsFilter.restore)
+        //.pipe(cssFilter)
+        //.pipe(concat('vendor.css'))
+        //.pipe(gulp.dest(dist.css))
+        //.pipe(cssFilter.restore)
+/*        .pipe(rename(function(path) {
+            if (~path.dirname.indexOf('fonts')) {
+                path.dirname = '/fonts'
+            }
+        }))*/
+        .pipe(gulp.dest(dist.vendor))
+})
+
+
+gulp.task('bower-css', function() {
+    var files = [
+        'bower_components/angular-gridster/dist/angular-gridster.min.css',
+        'bower_components/nvd3/build/nv.d3.min.css',
+        'bower_components/angular-material/angular-material.min.css',
+    ]
+
+    return gulp.src(files)
+        .pipe(concat('vendor.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest(dist.css))
 })
