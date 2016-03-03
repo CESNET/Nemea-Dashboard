89f2c20449d8c735940adbdd362a815f3beb4069
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-03-03 17:54:46 +0100

    New targets for js with no sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,20 @@ var bower = require('main-bower-files');
 var cssnano = require('gulp-cssnano');
 
 gulp.task('js', function () {
-gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+gulp.src(['js/**/*.js', '!js/**/*.min.js','!js/config.js'])
     .pipe(sourcemaps.init())
     .pipe(concat('js/app.min.js'))
     .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'))
+})
+
+gulp.task('js-nosm', function () {
+gulp.src(['js/**/*.js', '!js/**/*.min.js', '!js/config.js'])
+    .pipe(concat('js/app.min.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
     .pipe(gulp.dest('.'))
 })
 
