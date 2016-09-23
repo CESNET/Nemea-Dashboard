f3b5899725717c14443f0ac9d7161f4b49cfbc08
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-09-23 12:16:05 +0200

    Use configParser for configuration loading
    Drop JSON config files in favour of .ini format.
    
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- api/config.py
+++ api/config.py
@@ -1,12 +1,46 @@
-import os
-import json
-class Config:
-    def __init__(self):
-        self.raw_data = open(os.path.dirname(os.path.abspath(__file__)) + "/config.json").read()
-        self.data = json.loads(self.raw_data)
-        self.create_urls()
+import configparser
+from api import app
 
-    def create_urls(self):
-        self.data['events'] = '/' + self.data['api']['version'] + '/events/'
-        self.data['users'] = '/' + self.data['api']['version'] + '/users/'
+class Config(object):
+	"""
+	@class Config
+	@brief Handle configuration properties and set default values
+	"""
 
+	PROPAGATE_EXCEPTIONS = True
+
+	version = 'v1'
+
+	def __init__(self, args):
+		"""
+		Load configuration
+		"""
+		app.logger.debug("Loading user configuration")
+		self.config = configparser.ConfigParser()
+
+		self.config.read(args['config'])
+
+		self.DEBUG = self.config["api"].getboolean("debug", True)
+		self.HOST = self.config["api"].get("host", "localhost")
+		self.PORT = self.config["api"].getint("port", 8000)
+		self.THREADED = self.config["api"].getboolean("threaded", True)
+		self.SECRET_KEY = self.config["api"].get("secret_key", "")
+
+		self.create_urls()
+
+	def create_urls(self):
+		"""
+		Create URIs for main parts of API
+		* events Construct a base URI for events
+		*
+		"""
+		self.events = '/' + self.version + '/events/'
+		self.users = '/' + self.version + '/users/'
+
+	def __getitem__(self, key):
+		if key == "users":
+			return self.users
+		elif key == "events":
+			return self.users
+		else:
+			return self.config[key]
