f4bd1da74e767c55cf578f71740a07263c903b2c
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-09-23 12:17:43 +0200

    Use new config format for DB connector
    Signed-off-by: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>

--- api/dbConnector.py
+++ api/dbConnector.py
@@ -1,62 +1,70 @@
 import pymongo
-import sys
-from config import Config
-
-
-config = Config()
-C = config.data
-
+from api import app
 
 class dbConnector(object):
-    host = C["db"]["host"]
-    port = C["db"]["port"]
-    dbName = C["db"]["database"]
-    eventsCollection = C["db"]["collection"]["events"]
-    usersCollection = C["db"]["collection"]["users"]
-    sessionsCollection = C["db"]["collection"]["sessions"]
-    collection = None
-    users = None
-    events = None
-    sessions = None
-    db = None
-    socket = None
+	host = ""
+	port = ""
+	dbName = ""
+	eventsCollection = ""
+	usersCollection = ""
+	sessionsCollection = ""
+	collection = None
+	users = None
+	events = None
+	sessions = None
+	db = None
+	socket = None
 
-    def __init__(self, host = None, port = None, db = None, events = None, users = None, sessions = None):
+	@classmethod
+	def from_object(self, config):
+		return self(host = config.get("host", "localhost"),
+				port = config.getint("port", 27017),
+				db = config["database"],
+				events = config["events"],
+				users = config["users"],
+				sessions = config["sessions"])
 
-        # Setup host and port for DB 
-        if host != None and port != None:
-            self.host = host
-            self.port = port
-        
-        # Set up database name
-        if db != None:
-            self.dbName = db
-        
-        # Set up events collection
-        if events != None:
-            self.eventsCollection = events
+	def __init__(self, host = "localhost",
+			port = 27017,
+			db = "nemeadb",
+			events = "events",
+			users = "users",
+			sessions = "sessions"):
 
-        # Set up users collection
-        if users != None:
-            self.usersCollection = users
+		# Setup host and port for DB
+		self.host = host
+		self.port = port
 
-        # Connect to database and bind events and users collections 
-        try:
-            self.socket = pymongo.MongoClient(self.host, self.port, serverSelectionTimeoutMS=100)
-            
-            # Try to print out server info
-            # This raises ServerSelectionTimeoutError
-            self.socket.server_info()
-            
-            self.db = self.socket[self.dbName]
-            self.collection = self.events = self.db[self.eventsCollection]
-            self.users = self.db[self.usersCollection]
-            self.sessions = self.db[self.sessionsCollection]
+		# Set up database name
+		self.dbName = db
 
-        # Small trick to catch exception for unavailable database
-        except pymongo.errors.ServerSelectionTimeoutError as err:
-            print(err)
-            sys.exit()
+		# Set up events collection
+		self.eventsCollection = events
 
+		# Set up users collection
+		self.usersCollection = users
+
+		self.sessionsCollection = sessions
+
+		# Connect to database and bind events and users collections
+		try:
+			self.socket = pymongo.MongoClient(self.host,
+					self.port,
+					serverSelectionTimeoutMS=100)
+
+			# Try to print out server info
+			# This raises ServerSelectionTimeoutError
+			self.socket.server_info()
+
+			self.db = self.socket[self.dbName]
+			self.collection = self.events = self.db[self.eventsCollection]
+			self.users = self.db[self.usersCollection]
+			self.sessions = self.db[self.sessionsCollection]
+
+		# Small trick to catch exception for unavailable database
+		except pymongo.errors.ServerSelectionTimeoutError as err:
+			app.logger.error("Failed to connect to database: " + str(err))
+			print(err)
+			exit(1)
 #END db
 
