383d1a912f7aed4fb255912ec8c4245476660da3
Tomas Cejka <cejkat@cesnet.cz>
2016-10-04 11:36:56 +0200

    BUGFIX: replace "host" with "hostname"
    Since "host" contains port number, final generated URL may be
    http://<hostname>:<port>:5555 when the web server is originally accessed
    using <hostname>:<port>.
    
    This comes true e.g. using vagrant VM, where we are forwarding web to
    2280/TCP.  URL is http://localhost:2280/ and JavaScript generates URL
    during login in form of: http://localhost:2280:5555/ that is definitely
    wrong.

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 app.constant('SETUP', true);
 app.constant('CONFIG', {
-    "host" : "http://" + window.location.host,
+    "host" : "http://" + window.location.hostname,
     "port" : "5555",
     "version" : "v2"
 });
