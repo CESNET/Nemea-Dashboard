3870205deeb0059a98cdbb66fbf6f5ad06e8bc11
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-13 10:37:19 +0200

    NEMEA: get all available reporters

--- api/modules/nemea/reporter-rest/base.py
+++ api/modules/nemea/reporter-rest/base.py
@@ -1,20 +1,33 @@
 from liberouterapi import auth
 import json
+import re
 
 from liberouterapi.modules.nemea_status import get_topology
+
+# Regex to find the argument --config and its content
+config_regex = r"(?<=--config=)(.*)\s"
 
 def get_reporters():
     """Fetch all reporters using NEMEA Supervisor's supcli
     """
+    reporters = []
     try:
         res = get_topology()
+
+        # Output only *2idea modules and try to find the config file path
+        for module in res:
+            if "2idea" in module[0]:
+                m = re.search(config_regex, module[1]["params"])
+                if m is not None:
+                    module[1]["config_path"] = m.group(1)
+                reporters.append(module)
+        return(reporters)
     except Exception as e:
         print(e)
         return(json.dumps({
             "error" : True,
             "msg" : str(e)
             }))
-    return("")
 
 @auth.required()
 def protected_hello():
