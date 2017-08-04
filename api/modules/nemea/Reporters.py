696126b3d26001235644f052b4771b788e03896a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-08-04 13:36:59 +0200

    Remove dependency for reporter_conf from Reporters

--- api/modules/nemea/Reporters.py
+++ api/modules/nemea/Reporters.py
@@ -9,37 +9,38 @@ Path to configuration is specified in config.ini in this folder.
 """
 from liberouterapi import config
 from liberouterapi.error import ApiException
-from reporter_config.Parser import Parser
 from bson import json_util as json
 import yaml
 from flask import request
 
-
 class ReporterError(ApiException):
-	status_code = 400
+    status_code = 500
 
 if 'reporters_config' not in config.modules['nemea']:
-	raise ReporterError("missing path to reporters configuration file 'reporters_config'")
+    raise ReporterError("missing path to reporters configuration file 'reporters_config'")
 
 import os
 
 def get_nr_config():
-	rconf = None
-	try:
-		with open(config.modules['nemea']['reporters_config']) as f:
-			rconf = Parser.load(f)
-	except FileNotFoundError as e:
-		raise ReporterError("File %s not found" % config.modules['nemea']['reporters_config'],
-				status_code = 404)
-	except Exception as e:
-		raise ReporterError(str(e))
+    rconf = None
+    try:
+        with open(config.modules['nemea']['reporters_config']) as f:
+            try:
+                rconf = yaml.load(f)
+            except Exception as e:
+                raise ReporterError("Error while parsing config file")
+    except FileNotFoundError as e:
+        raise ReporterError("File %s not found" % config.modules['nemea']['reporters_config'],
+                status_code = 404)
+    except Exception as e:
+        raise ReporterError(str(e))
 
-	return(json.dumps(rconf))
+    return(json.dumps(yaml.dump(rconf)))
 
 def edit_nr_config():
-	conf = request.get_json()
-	with open(config.modules['nemea']['reporters_config'], 'w') as yf:
-		yaml.dump(conf, yf, default_flow_style=False, indent = 4)
+    conf = request.get_json()
+    with open(config.modules['nemea']['reporters_config'], 'w') as yf:
+        yaml.dump(conf, yf, default_flow_style=False, indent = 4)
 
-	return json.dumps(conf)
+    return json.dumps(conf)
 
